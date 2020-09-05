3bbf12c24229b7a8df925032c4e15b722b80da93
thiagosilva <thiago.silva@imd.ufrn.br>
2020-09-05 20:55:09 -0300

    Styled components removido

--- src/styles/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-
-import Theme from './theme'
-
-export type Theme = typeof Theme
-
-declare module 'styled-components' {
-  export interface DefaulTheme extends Theme { }
-}
